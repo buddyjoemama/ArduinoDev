491029b3fccba881dd91de96c9dfcd911f975770
Brian Bacon <brian.bacon01@gmail.com>
2013-02-03 17:26:34 -0500

    Updated c3 helpers

--- ProgramUsingBootloader.bat
+++ ProgramUsingBootloader.bat
@@ -1,1 +1,2 @@
-avrdude -c avr109 -p m32u4 -P COM10 -U %1 -D
+C:\ArduinoDev\ArduinoDev\ResetToBootloader.exe %2
+avrdude -c avr109 -p m32u4 -P %3 -U %1 -D
---
+++ Projects/C#/ResetApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ResetToBootloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No serial port defined. Returning.");
+                return;
+            }
+
+            Console.WriteLine("Resetting Arduino. Using port {0}...", args[0]);
+
+            try
+            {
+                SerialPort p = new SerialPort(args[0], 1200);
+                p.Open();
+                p.Close();
+            }
+            catch { }
+
+            Thread.Sleep(5500);
+            Console.WriteLine("Arduino reset into bootloader!");
+
+        }
+    }
+}
